f64396fba19827206af747edf7c6ef2e30d22bee
YneNuh <165549081+Ynehnu23@users.noreply.github.com>
2024-05-31 11:00:51 +0700

    Update two_phase.py

--- two_phase.py
+++ two_phase.py
@@ -109,9 +109,9 @@ def PrintConstraints(a, operators, b, conditions):
 
 def convert_to_standard_form(a, b, c, n, m, problem_type, operators, conditions):
      # Kiểm tra đầu vào hợp lệ
-    if not isinstance(n, int) hoặc n <= 0:
+    if not isinstance(n, int) or n <= 0:
         raise ValueError("Số lượng ràng buộc 'n' phải là một số nguyên dương.")
-    if not isinstance(m, int) hoặc m <= 0:
+    if not isinstance(m, int) or m <= 0:
         raise ValueError("Số lượng biến 'm' phải là một số nguyên dương.")
     print("Bài toán có sau khi chuyển đổi:")
     new_c = []
